03003b05d7168c77e8671b6522aa9fa4f689bf18
pttkachuk <petrotkachuk6@gmail.com>
2023-07-05 15:21:50 +0200

    bug fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,30 @@
 import PropTypes from 'prop-types';
 const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => {
     return (
-        <div class="profile">
-  <div class="description">
+        <div className="profile">
+  <div className="description">
     <img
       src={avatar}
       alt="User avatar"
-      class="avatar"
+      className="avatar"
     />
-    <p class="name">{username}</p>
-    <p class="tag">@{tag}</p>
-    <p class="location">{location}</p>
+    <p className="name">{username}</p>
+    <p className="tag">@{tag}</p>
+    <p className="location">{location}</p>
   </div>
 
-  <ul class="stats">
+  <ul className="stats">
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{followers}</span>
+      <span className="label">Followers: </span>
+      <span className="quantity">{followers}</span>
     </li>
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">{views}</span>
+      <span className="label">Views: </span>
+      <span className="quantity">{views}</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">{likes}</span>
+      <span className="label">Likes: </span>
+      <span className="quantity">{likes}</span>
     </li>
   </ul>
 </div>
