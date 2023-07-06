f18e22844300e7f687f49c268493a67fc969ed80
pttkachuk <petrotkachuk6@gmail.com>
2023-07-06 20:16:39 +0200

    add scale animation on hover task 03

--- src/components/FriendList/FriendStyle.js
+++ src/components/FriendList/FriendStyle.js
@@ -7,7 +7,13 @@ padding: 20px;
 border-radius: 10px;
 max-width:280px;
 margin-bottom: 10px;
+cursor: pointer;
+transition: transform 250ms cubic-bezier(0.56, 0.57, 0.76, 0.76);
 box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+
+&:hover{
+    transform: scale(1.1);
+}
 `;
 
 export const StatusSpan = styled.span`
